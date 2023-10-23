8c3fc6a83208c993b6a7aa1f0c3b5986cc368325
x-i-f-r-a <gokulkrishnan.5058@gmail.com>
2023-10-23 11:22:16 +0530

    Minor changes

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
-cd jain;
+cd ics-international;
 pm2 stop index.js;
 cd;
-git clone https://github.com/x-i-f-r-a/jain.git;
-cd jain;
+git clone https://github.com/x-i-f-r-a/ics-international.git;
+cd ics-international;
 pm2 restart index.js;
 systemctl restart nginx;
 echo "\nConfiguration done and server restarted" 
